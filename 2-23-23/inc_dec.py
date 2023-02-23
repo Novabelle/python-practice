2e78acabacd17ff2b1e772bc48e7b2cdfe225e3a
Novabelle <toosketchyyy@gmail.com>
2023-02-23 17:53:59 -0500

    increment/decrement tests

---
+++ 2-23-23/inc_dec.py
@@ -0,0 +1,9 @@
+def increment(x):
+    """accepts argument x, and returns x + 1"""
+    result = x + 1
+    return result
+
+def decrement(x):
+    """accepts argument x, and returns x - 1"""
+    result = x - 1
+    return result
